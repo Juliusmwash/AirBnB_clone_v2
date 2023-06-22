3a5b972624fc3050f10d8fd4811d8614b0a2fabb
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-22 22:11:45 +0300

    Redo the project again

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
+from uuid import uuid4
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """A base class for all hbnb models"""
+    def __init__(self, *args, **kwargs):
+        """Instanciates a new model.
+        """
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    value = datetime.utcnow()
+                if key != "__class__":
+                    setattr(self, key, value)
+        storage.new(self)
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
