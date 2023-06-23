15a848ee08ab777ced0424ef41f1799234921e65
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-23 15:13:36 +0300

    Figure out how to automatically setup the database for the project

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -15,6 +15,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import reflection
+from sqlalchemy.exc import OperationalError
 
 
 class DBStorage:
@@ -36,6 +37,7 @@ class DBStorage:
                 getenv("HBNB_MYSQL_HOST"),
                 getenv("HBNB_MYSQL_DB"))
         self.__engine = create_engine(db_url, pool_pre_ping=True)
+        self.create_database()
 
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
@@ -67,15 +69,43 @@ class DBStorage:
 
         return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
 
+    def setup_database(self):
+        """Create the engine"""
+        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(
+                getenv("HBNB_MYSQL_USER"),
+                getenv("HBNB_MYSQL_PWD"),
+                getenv("HBNB_MYSQL_HOST"),
+                getenv("HBNB_MYSQL_DB"))
+        self.__engine = create_engine(db_url, pool_pre_ping=True)
+
+        try:
+            # Execute a sample query to test the connection
+            with self.__engine.connect() as connection:
+                result = connection.execute("SELECT 1")
+
+        except OperationalError as e:
+            # Catch and handle the SQLAlchemy OperationalError
+            if e.orig.args and e.orig.args[0] == 1049:
+                # Recreate the database dynamically
+                self.create_database()
+
+                # Reattempt to connect to the newly created database
+                self.__engine.dispose()
+                self.__engine = create_engine(db_url, pool_pre_ping=True)
+            else:
+                print("Error:", e)
+
     def create_database(self):
-        inspector = reflection.Inspector.from_engine(engine)
+        inspector = reflection.Inspector.from_engine(self.__engine)
         if getenv('HBNB_ENV') == "dev":
             database_exists = inspector.has_database('hbnb_dev_db')
             if not database_exists:
                 with open('setup_mysql_dev.sql', 'r') as script_file:
                     sql_script = script_file.read()
                     print("sql_script :\n{}".format(sql_script))
-                engine.execute(sql_script)
+                    self.__engine.execute(sql_script)
+                self.__engine.dispose()
+                self.__engine = create_engine(db_url, pool_pre_ping=True)
             else:
                 print("database hbnb_dev_db exists")
         else:
@@ -84,7 +114,9 @@ class DBStorage:
                 with open('setup_mysql_test.sql', 'r') as script_file:
                     sql_script = script_file.read()
                     print("sql_script :\n{}".format(sql_script))
-                engine.execute(sql_script)
+                    self.__engine.execute(sql_script)
+                self.__engine.dispose()
+                self.__engine = create_engine(db_url, pool_pre_ping=True)
             else:
                 print("database hbnb_test_db exists")
 
