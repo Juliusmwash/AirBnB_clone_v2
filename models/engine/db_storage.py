35220c7eb90b913a25d69c393018d1e020f3d328
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-25 05:36:36 +0300

    Uncomment a line in db_storage module

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -56,7 +56,7 @@ class DBStorage:
             objs.extend(self.__session.query(User).all())
             objs.extend(self.__session.query(Place).all())
             objs.extend(self.__session.query(Review).all())
-            #objs.extend(self.__session.query(Amenity).all())
+            objs.extend(self.__session.query(Amenity).all())
         else:
             try:
                 cls = globals()[cls]
