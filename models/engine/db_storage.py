45594793d3d6cdbfb62b3a3d16e2710935e696dc
Juliusmwash <cutejuliusmwash@gmail.com>
2023-06-25 05:34:17 +0300

    Add extra features to Review and Amenity classes

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -55,7 +55,7 @@ class DBStorage:
             objs.extend(self.__session.query(City).all())
             objs.extend(self.__session.query(User).all())
             objs.extend(self.__session.query(Place).all())
-            #objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Review).all())
             #objs.extend(self.__session.query(Amenity).all())
         else:
             try:
--- models/place.py
+++ models/place.py
@@ -9,6 +9,20 @@ from sqlalchemy import Column, Float, ForeignKey, Integer, String, Table
 from sqlalchemy.orm import relationship
 
 
+place_amenity = Table(
+        'place_amenity',
+        Base.metadata,
+        Column('place_id', String(60),
+               ForeignKey('places.id'),
+               primary_key=True,
+               nullable=False),
+        Column('amenity_id', String(60),
+               ForeignKey('amenities.id'),
+               primary_key=True,
+               nullable=False)
+        )
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
 
@@ -23,6 +37,9 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
+    reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                             viewonly=False)
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
@@ -33,14 +50,39 @@ class Place(BaseModel, Base):
         super().__init__(**kwargs)
         from models.user import User
         from models.city import City
-        if getenv("HBNB_TYPE_STORAGE") == "db":
+        if getenv("HBNB_TYPE_STORAGE", None) == "db":
             users = models.storage.all(User)
             cities = models.storage.all(City)
-            for key, obj in users.items():
+            for obj in users.values():
                 if obj.id == self.user_id:
                     self.user = obj
                     break
-            for key, obj in cities.items():
+            for obj in cities.values():
                 if obj.id == self.city_id:
                     self.cities = obj
                     break
+
+    if getenv("HBNB_TYPE_STORAGE", None) != "db":
+        @property
+        def reviews(self):
+            """Get a list of all linked Reviews."""
+            review_list = []
+            for review in list(models.storage.all(Review).values()):
+                if review.place_id == self.id:
+                    review_list.append(review)
+            return review_list
+
+        @property
+        def amenities(self):
+            """Get linked Amenities."""
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, value):
+            """add amenity id to amenity_ids"""
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
