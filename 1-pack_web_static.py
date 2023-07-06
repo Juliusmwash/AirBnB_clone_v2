cde606e38d067c342f05e9c83f161e7d73986f6d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-07 02:16:49 +0300

    Add some safety measure when versions directory is being created

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+module defining do_pack function
+"""
+from os.path import isdir
 from fabric.api import local
 from datetime import datetime
 
@@ -8,14 +12,13 @@ def do_pack():
     Generates a .tgz archive from the contents of the web_static
     folder of AirBnB Clone repo
     """
-    local('mkdir -p versions')
-
+    if isdar("versions") is False:
+        if local('mkdir -p versions').failed is True:
+            return None
     now = datetime.now()
     timestamp = now.strftime('%Y%m%d%H%M%S')
     archive_path = 'versions/web_static_{}.tgz'.format(timestamp)
-
     result = local('tar -czvf {} web_static'.format(archive_path))
-
     if result.succeeded:
         return archive_path
     else:
