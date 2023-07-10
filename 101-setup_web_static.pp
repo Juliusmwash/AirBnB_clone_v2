0ab218d56836e521e327fbbc5527ffc7a62be589
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-10 05:01:58 +0300

    Add sudo mode

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -2,6 +2,6 @@
 
 exec {'configure_server':
   provider => shell,
-  command => 'git clone https://github.com/Juliusmwash/AirBnb_clone_v2.git ; mv AirBnb_clone_v2/0-setup_web_static.sh . ; ./0-setup_web_static.sh,
-  path => '/usr/bin:/bin:/path/to',
+  command => 'git clone https://github.com/Juliusmwash/AirBnb_clone_v2.git ; sudo mv AirBnb_clone_v2/0-setup_web_static.sh . ; sudo ./0-setup_web_static.sh,
+  path => '/usr/bin:/bin',
 }
